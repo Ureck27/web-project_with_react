d732251ce3e2dcfb5a150aab88e41690072953aa
rachid aourik <rachidaourik18@gmail.com>
2025-11-25 22:54:47 +0100

    edit navbar

--- src/Componment/Navbar/Navbar.jsx
+++ src/Componment/Navbar/Navbar.jsx
@@ -3,6 +3,12 @@ function Navbar(){
   return (
     <div>
        <h1>hello world!(this is Navbar)</h1>
+       <ul>
+          <li><a href="">Home</a></li>
+          <li><a href="">about</a></li>
+          <li><a href="">contact us</a></li>
+
+       </ul>
     </div>
   )
 }
