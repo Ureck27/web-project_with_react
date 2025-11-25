877a8b10f4e81f6254d1d975277dfad3f03bef3f
rachid aourik <rachidaourik18@gmail.com>
2025-11-25 21:53:05 +0100

    first_commit

---
+++ src/Componment/Footer/Footer.jsx
@@ -0,0 +1,15 @@
+function footer(){
+   return ( <div>
+    h1
+    <ol>
+        <li><a href="www.facebbok.com">facebbok</a></li>
+        <li><a href="www.google.com">Google</a></li>
+        <li><a href="www.instagrame.com">instagrame</a></li>
+        <li><a href="www.x.com">tweater</a></li>
+
+    </ol>
+   </div> )
+}
+
+
+export default footer ;
