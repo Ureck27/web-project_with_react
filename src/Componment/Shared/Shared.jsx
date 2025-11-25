b5c941e66f369a81ff2d18eeb411bc32e234cfd5
rachid aourik <rachidaourik18@gmail.com>
2025-11-25 22:43:23 +0100

    Shared

---
+++ src/Componment/Shared/Shared.jsx
@@ -0,0 +1,16 @@
+function Shared(){
+    return (
+        <div>
+            <section></section>
+            <form action="">
+                <p>Enter your email</p>
+                <input type="text" />
+                <p>Entre Your message</p>
+                <input type="text" />
+                <button></button>
+            </form>
+        </div>
+    )
+}
+
+export default Shared ;
--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,12 @@ import './index.css'
 import App from './App.jsx'
 import Footer from './Componment/Footer/Footer.jsx'
 import Navbar from './Componment/Navbar/Navbar.jsx'
+import Shared from './Componment/Shared/Shared.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Navbar />
     <Footer />
+    <Shared />
   </StrictMode>,
 )
