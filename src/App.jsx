dee19b349b01ed27b8f82b19fd92cafcfa81e8ac
rachid aourik <rachidaourik18@gmail.com>
2025-11-25 22:09:20 +0100

    Footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,12 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import footer from './Componment/Footer/Footer'
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-    
+    <footer />
     </>
   )
 }
--- src/Componment/Footer/Footer.jsx
+++ src/Componment/Footer/Footer.jsx
@@ -1,13 +1,13 @@
 function footer(){
    return ( <div>
-    h1
-    <ol>
+    <h1>this is my footer</h1>
+    <ul>
         <li><a href="www.facebbok.com">facebbok</a></li>
         <li><a href="www.google.com">Google</a></li>
         <li><a href="www.instagrame.com">instagrame</a></li>
         <li><a href="www.x.com">tweater</a></li>
 
-    </ol>
+    </ul>
    </div> )
 }
 
