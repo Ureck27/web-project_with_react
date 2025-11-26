6a88a7a2a148d03dd670f9606cf2f716fea30f13
rachid aourik <rachidaourik18@gmail.com>
2025-11-26 11:49:25 +0100

    00

--- src/Componment/Footer/Footer.jsx
+++ src/Componment/Footer/Footer.jsx
@@ -1,15 +1,20 @@
 function footer(){
-   return ( <div>
+   return ( 
+    <>
+   <div>
     <h1>this is my footer</h1>
-    <ul>
+     <ul>
         <li><a href="www.facebbok.com">facebbok</a></li>
         <li><a href="www.google.com">Google</a></li>
         <li><a href="www.instagrame.com">instagrame</a></li>
         <li><a href="www.x.com">tweater</a></li>
 
     </ul>
-   </div> )
-}
+   </div> 
+   </>
+   )
+};
+
 
 
 export default footer ;
