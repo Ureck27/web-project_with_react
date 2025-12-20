77f3b766d9448df3ea98f96edf21892b6f792d89
rachid aourik <rachidaourik18@gmail.com>
2025-12-20 19:38:29 +0100

    add api

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,22 @@
-import Favorite from "./pages/Favorites";
+import "./css/App.css";
+import Favorites from "./pages/Favorites";
 import Home from "./pages/Home";
-import { Routes,Route } from "react-router-dom";
-import NavBar from "./componments/NavBar";
-function App() {
+import { Routes, Route } from "react-router-dom";
+import { MovieProvider } from "./contexts/MovieContext";
+import NavBar from "./components/NavBar";
 
+function App() {
   return (
-    <div>
-       <NavBar />
-       <main className="main-content">
+    <MovieProvider>
+      <NavBar />
+      <main className="main-content">
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/favorites" element={<Favorite />} />
-          
-
+          <Route path="/favorites" element={<Favorites />} />
         </Routes>
-       </main>
-       </div>
-  )
+      </main>
+    </MovieProvider>
+  );
 }
-
 
 export default App;
