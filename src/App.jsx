2d4f55a07435eb0af550a1ace2fff5f2f1bf4839
rachid aourik <rachidaourik18@gmail.com>
2025-12-20 12:20:22 +0100

    delet old componment

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Navbar from './Componment/Navbar/Navbar'
-import Slider from './Componment/Slider/Slider'
-import Shared from './Componment/Shared/Shared'
-import Home from '../Pages/Home'
+
 function App() {
 
   return (
--- src/Componment/Slider/Slider.jsx
+++
@@ -1,7 +0,0 @@
-function Slider(){
-    return (
-       <h1>hello world</h1>
-    )
-}
-
-export default Slider ;
