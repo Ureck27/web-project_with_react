8b2ed8bb9b617e6095efbfacac87abfa215fe432
rachid aourik <rachidaourik18@gmail.com>
2025-12-20 15:46:48 +0100

    browser route /favorites

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
+import Favorite from "./pages/Favorites";
 import Home from "./pages/Home";
+import { Routes,Route } from "react-router-dom";
 function App() {
 
   return (
-         <>
-          <Home />
-         </>
+       <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/favorites" element={<Favorite />} />
+          
+
+        </Routes>
+       </main>
   )
 }
 
