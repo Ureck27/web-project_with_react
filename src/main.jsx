c293a265cdadf1b41efd0d51f02bbccf07916f5e
rachid aourik <rachidaourik18@gmail.com>
2025-12-20 12:21:43 +0100

    remve old css

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import Footer from './Componment/Footer/Footer.jsx'
-import Navbar from './Componment/Navbar/Navbar.jsx'
-import Shared from './Componment/Shared/Shared.jsx'
-import Slider from './Componment/Slider/Slider.jsx'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
